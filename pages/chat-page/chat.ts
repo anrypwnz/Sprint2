00c668601d5bbbd8779e643086f5dd486de4e90d
unknown <andri@andrianov.corp.okbmei.ru>
2021-07-23 16:34:23 +0300

    ts

--- pages/chat-page/chat.ts
+++ pages/chat-page/chat.ts
@@ -2,12 +2,12 @@ import './chat-page.less'
 import '../../src/styles/styles.less'
 import '../../src/styles/general.less'
 
+const infoPanelButton: HTMLElement = document.querySelector('.info-panel__menu-btn')!
 
-document.querySelector('.info-panel__menu-btn').addEventListener('mousedown', () => {
-    document.querySelector('.info-panel__menu').classList.toggle('modal-options_show')
+infoPanelButton.addEventListener('mousedown', () => {
+    document.querySelector('.info-panel__menu')!.classList.toggle('modal-options_show')
 })
 
-document.querySelector('.msg-panel__add-file').addEventListener('mousedown', () => {
-    document.querySelector('.modal-options').classList.toggle('modal-options_show')
+document.querySelector('.msg-panel__add-file')!.addEventListener('mousedown', () => {
+    document.querySelector('.modal-options')!.classList.toggle('modal-options_show')
 })
-
--- pages/profile/profile.ts
+++ pages/profile/profile.ts
@@ -1,3 +1,2 @@
 import '../../src/styles/styles.less'
 import '../login/login.less'
-
